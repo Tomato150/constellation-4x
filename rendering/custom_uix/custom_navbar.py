7cd5cdac32e74c9c838a476539186028b0ad5711
BuildTools <unconfigured@null.spigotmc.org>
2017-04-07 09:29:40 +1000

    Small edits

--- rendering/custom_uix/custom_navbar.py
+++ rendering/custom_uix/custom_navbar.py
@@ -73,4 +73,4 @@ class CustomNavbar(StackLayout, CanvasEnabled):
         elif gap_widget:
             gap_widget.width = 0
 
-        Clock.schedule_once(self.background_canvas_resize, 0)
+        Clock.schedule_once(self.background_canvas_resize, -1)
